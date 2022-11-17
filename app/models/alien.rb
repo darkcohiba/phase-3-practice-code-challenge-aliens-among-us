7e2e52f4ca10366cfb9225f4acb34a7a356045fc
Sam <samuel.waters@gmail.com>
2022-11-16 17:56:55 -0700

    finished

--- app/models/alien.rb
+++ app/models/alien.rb
@@ -1,3 +1,4 @@
 class Alien < ActiveRecord::Base
-
+    has_many :visitations, dependent: :destroy
+    has_many :earthlings, through: :visitations
 end
--- app/models/earthling.rb
+++ app/models/earthling.rb
@@ -1,3 +1,5 @@
 class Earthling < ActiveRecord::Base
+    has_many :visitations, dependent: :destroy
+    has_many :aliens, through: :visitations
 
 end
